56e9393264312ea75c82fa365c096b156a221107
João R. Afonso <85640066+jrochafonso@users.noreply.github.com>
2022-08-09 00:35:29 +0100

    dunno if getting closer or farther

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const cells = $(".cell");
 const board = $(".board");
-const winMessage = $("winning-message");
+const winMessage = $(".winning-message-text");
+const winDisplay = $(".winning-message");
 let is_o;
 
 const winComb = [   // lista de arrays (estamos a trabalhar com arrays);
@@ -23,61 +24,46 @@ const startGame = () => {
 
     board.addClass('x'); //jq
 }
-/*
-const endGame = () => {
+
+const endGame = (isDraw) => {
     if (isDraw){
-        winMessage = "empate!";
+        winMessage.innerText = "empate!";
     } else {
-        winMessage = "winner!";
+        winMessage.innerText = is_o ? "o venceu!" : "x venceu!";
     }
-    winMessage.addClass('show-winning-message'); //jq
+    winDisplay.addClass('show-winning-message'); //jq
+};
 
-}*/
-
-//se houver checkwin acabo o jogo
-//senão troco jogador
-
-const check4Win = () => {
+const check4Win = (class2Add) => {
     for (let i = 0; i < winComb.length; i++) {
         const cell1 = cells[winComb[i][0]]
         const cell2 = cells[winComb[i][1]]
         const cell3 = cells[winComb[i][2]]
 
-        // check x
         if (
-            cell1.classList.contains('x') &&
-            cell2.classList.contains('x') &&
-            cell3.classList.contains('x')
+            cell1.classList.contains(class2Add) &&
+            cell2.classList.contains(class2Add) &&
+            cell3.classList.contains(class2Add)
         )
         {
-            console.log("x wins");
-        }
-        // check o
-        if (
-            cell1.classList.contains('o') &&
-            cell2.classList.contains('o') &&
-            cell3.classList.contains('o')
-        )
-        {
-            console.log("o wins");
+            console.log(`player ${class2Add} wins!`);
+            return class2Add;
         }
     }
-}
+};
 
-/*
-const check4Win = (currentPlayer) => {
-    return winComb.$(winComb).val(comb => {
-        return comb.every
-
-    })
-}
-*/
+const check4Draw = () => {
+    //     if (cells.x.length + cells.o.length === 9) {
+    if (cells.x.length === 4) {
+        return true;
+    }
+};
 
 const placeXO = (cell, class2Add) => { // adicionar x ou o
     if(!(cell.classList.contains('o' && 'x'))){ // checks if cell is used
         cell.classList.add(class2Add); //js
     }
-}
+};
 
 const swapUP = () => {
     is_o = !is_o;
@@ -90,20 +76,42 @@ const swapUP = () => {
     } else {
         board.addClass('x'); //jq
     }
-}
+};
 
 const handleClick = (e) => {    // recebe elemento da cell
     const cell = e.target; //js
     const class2Add = is_o ? 'o' : 'x'; // mudar o símbolo
     placeXO(cell, class2Add);
-    check4Win();
+
+    const isWin = check4Win(class2Add)
+    const isDraw = check4Draw();
+
+    if (isWin) {
+        console.log("ganhou");
+        endGame(false);
+    } else if (isDraw) {
+        console.log("empate");
+        endGame(true);
+    } else {
+        swapUP();
+    }
+
+    //endGame(false);
     swapUP();
-}
+};
 
 startGame();
 
 
 
+/*
+const check4Win = (currentPlayer) => {
+    return winComb.$(winComb).val(comb => {
+        return comb.every
+
+    })
+}
+*/
 
 
 // placeXO: done w/check
